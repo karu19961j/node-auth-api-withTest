a91e3218ee70b78f1440b0d21598937e48c05cad
Karan Pahwa <karan@Karans-MacBook-Pro.local>
2019-01-28 00:45:19 +0530

    in progress login api

--- server/server.js
+++ server/server.js
@@ -35,6 +35,17 @@ app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post("/users/login", (req, res) => {
+  let body = _.pick(req.body, ["email", "password"]);
+  User.findByCredentials(body.email, body.password)
+    .then(user => {
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
