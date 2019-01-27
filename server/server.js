72289b027af90cd15782c39ba1f01d5a013b6f05
Karan Pahwa <karan@Karans-MacBook-Pro.local>
2019-01-28 00:19:12 +0530

    all login test-cases

--- server/server.js
+++ server/server.js
@@ -21,7 +21,10 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(user => {
-      res.send(user);
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res.header("x-auth", token).send(user);
     })
     .catch(e => {
       res.status(400).send(e);
